f54b8273476a5280b20c92fc6942b0f42b9586a2
silvanshade <silvanshade@users.noreply.github.com>
2020-11-25 15:09:31 -0700

    Update grammar module; build external scanners

--- crates/parsers/build.rs
+++ crates/parsers/build.rs
@@ -7,12 +7,14 @@ fn compile_tree_sitter_grammars() -> anyhow::Result<()> {
     let mut cc = cc::Build::new();
     cc.include(dir.join("ddlog/dat/src"));
     cc.file(dir.join("ddlog/dat/src/parser.c"));
+    cc.file(dir.join("ddlog/dat/src/scanner.cc"));
     cc.compile("tree-sitter-ddlog-dat");
 
     println!("cargo:rerun-if-changed={:?}", dir.join("ddlog/dl/src/parser.c"));
     let mut cc = cc::Build::new();
     cc.include(dir.join("ddlog/dl/src"));
     cc.file(dir.join("ddlog/dl/src/parser.c"));
+    cc.file(dir.join("ddlog/dl/src/scanner.cc"));
     cc.compile("tree-sitter-ddlog-dl");
 
     Ok(())
