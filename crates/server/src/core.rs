11aac6f2609f91240e2fa66a40fccb472b1ca811
silvanshade <silvanshade@users.noreply.github.com>
2021-02-01 17:28:32 -0700

    Adjust exports

--- crates/server/src/core.rs
+++ crates/server/src/core.rs
@@ -6,7 +6,7 @@ mod rope;
 mod session;
 mod text;
 
-pub use ddlog_lsp_parsers::core::language::*;
+pub use ddlog_lsp_parsers::core::language::{self, Language};
 pub use document::*;
 pub use error::*;
 pub use rope::*;
