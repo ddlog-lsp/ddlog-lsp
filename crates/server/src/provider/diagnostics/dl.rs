0579a47643e548dca9108ed78aa9b763edfe18ba
silvanshade <silvanshade@users.noreply.github.com>
2021-02-21 17:24:27 -0700

    Split diagnostics provider

---
+++ crates/server/src/provider/diagnostics/dl.rs
@@ -0,0 +1,53 @@
+use crate::{core, util::NodeWalker};
+use lsp_text::RopeExt;
+
+pub fn diagnostics(tree: &tree_sitter::Tree, content: &ropey::Rope) -> Vec<lsp::Diagnostic> {
+    let mut diagnostics = vec![];
+    let mut walker = {
+        let language = core::Language::DDlogDl;
+        let node = tree.root_node();
+        NodeWalker::new(language, node)
+    };
+
+    loop {
+        if walker.done {
+            break;
+        }
+
+        let node = walker.node();
+
+        if node.is_error() {
+            let message = String::from("ERROR node");
+            let range = content.tree_sitter_range_to_lsp_range(node.range());
+            let severity = Some(lsp::DiagnosticSeverity::Error);
+            diagnostics.push(lsp::Diagnostic {
+                message,
+                range,
+                severity,
+                ..Default::default()
+            });
+            walker.goto_next_has_error();
+            continue;
+        }
+
+        if node.is_missing() {
+            let message = String::from("MISSING node");
+            let range = content.tree_sitter_range_to_lsp_range(node.range());
+            let severity = Some(lsp::DiagnosticSeverity::Error);
+            diagnostics.push(lsp::Diagnostic {
+                message,
+                range,
+                severity,
+                ..Default::default()
+            });
+            walker.goto_next_has_error();
+            continue;
+        }
+
+        // catch all case
+        walker.goto_next_has_error();
+    }
+
+    diagnostics.reverse();
+    diagnostics
+}
