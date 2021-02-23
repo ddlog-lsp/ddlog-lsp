407cd9c439cf19a6bcda21563adacdbe219e4d77
silvanshade <silvanshade@users.noreply.github.com>
2021-02-22 19:03:06 -0700

    Rename parsers crate

--- crates/server/src/core.rs
+++ crates/server/src/core.rs
@@ -5,7 +5,7 @@ mod error;
 mod session;
 mod text;
 
-pub use ddlog_lsp_parsers::language::{self, Language};
+pub use ddlog_lsp_syntax::language::{self, Language};
 pub use document::*;
 pub use error::*;
 pub use session::*;
