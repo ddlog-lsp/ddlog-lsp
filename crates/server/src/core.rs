f63b9d803be533421edeb136a50889654b7f4c67
silvanshade <silvanshade@users.noreply.github.com>
2021-10-14 10:49:42 -0600

    Define WorkspaceFolder

--- crates/server/src/core.rs
+++ crates/server/src/core.rs
@@ -4,6 +4,7 @@ mod document;
 mod error;
 mod session;
 mod text;
+mod workspace_folder;
 
 pub use ddlog_lsp_languages::{language::Language, parser};
 pub use ddlog_lsp_syntax::{language, node, range};
@@ -11,3 +12,4 @@ pub use document::*;
 pub use error::*;
 pub use session::*;
 pub use text::*;
+pub use workspace_folder::*;
---
+++ crates/server/src/core/workspace_folder.rs
@@ -0,0 +1,56 @@
+#[derive(Clone)]
+pub struct WorkspaceFolder(pub lsp::WorkspaceFolder);
+
+impl WorkspaceFolder {
+    pub fn uri(&self) -> &lsp::Url {
+        &self.0.uri
+    }
+
+    pub fn name(&self) -> &str {
+        self.0.name.as_str()
+    }
+}
+
+#[allow(unsafe_code)]
+unsafe impl Send for WorkspaceFolder {
+}
+
+#[allow(unsafe_code)]
+unsafe impl Sync for WorkspaceFolder {
+}
+
+impl Unpin for WorkspaceFolder {
+}
+
+impl std::fmt::Debug for WorkspaceFolder {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl std::cmp::Eq for WorkspaceFolder {
+}
+
+impl std::cmp::Ord for WorkspaceFolder {
+    fn cmp(&self, that: &Self) -> std::cmp::Ordering {
+        let this = self.0.uri.as_str();
+        let that = that.0.uri.as_str();
+        std::cmp::Ord::cmp(this, that)
+    }
+}
+
+impl std::cmp::PartialEq for WorkspaceFolder {
+    fn eq(&self, that: &Self) -> bool {
+        let this = self.0.uri.as_str();
+        let that = that.0.uri.as_str();
+        std::cmp::PartialEq::eq(this, that)
+    }
+}
+
+impl std::cmp::PartialOrd for WorkspaceFolder {
+    fn partial_cmp(&self, that: &Self) -> Option<std::cmp::Ordering> {
+        let this = self.0.uri.as_str();
+        let that = that.0.uri.as_str();
+        std::cmp::PartialOrd::partial_cmp(this, that)
+    }
+}
