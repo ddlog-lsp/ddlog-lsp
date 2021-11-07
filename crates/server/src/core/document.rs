a5e76f17c43f0bcfbc542216de94e4ae644b421d
silvanshade <silvanshade@users.noreply.github.com>
2021-11-07 13:42:39 -0700

    WIP

--- crates/server/src/core/document.rs
+++ crates/server/src/core/document.rs
@@ -17,51 +17,23 @@ pub struct DocumentFuture {
 }
 
 impl DocumentFuture {
-    pub async fn open_from_lsp(params: lsp::DidOpenTextDocumentParams) -> anyhow::Result<Self> {
+    pub fn open_from_lsp(params: lsp::DidOpenTextDocumentParams) -> anyhow::Result<Self> {
         let uri = params.text_document.uri;
-
         let language = crate::core::Language::try_from(params.text_document.language_id.as_str())?;
-
-        let mut parser = {
-            let language = language.clone();
-            let val = tokio::spawn(async move {
-                let val = tree_sitter::Parser::try_from(language);
-                if let Ok(parser) = val {
-                    Some(Arc::new(Mutex::new(parser)))
-                } else {
-                    None
-                }
-            });
-            let val = val.map(Result::ok).map(Option::flatten);
-            let val = EagerFuture::new(val);
-            val
-        };
-
+        let mut parser = tree_sitter::Parser::try_from(language)?;
         let content = ropey::Rope::from(params.text_document.text);
 
         let tree = {
-            let parser = parser.clone();
             let content = content.clone();
             let byte_idx = 0;
             let text = content.chunks().collect::<String>();
             let old_tree = None;
-            let val = tokio::spawn(async move {
-                if let Some(parser) = parser.await {
-                    let mut parser = parser.lock().await;
-                    parser.parse(text, old_tree).unwrap()
-                } else {
-                    None
-                }
-            });
-            let val = val.map(Result::ok).map(Option::flatten);
-            EagerFuture::new(val)
+            parser.parse(text, old_tree)?
         };
 
-        let content = {
-            let val = ropey::Rope::from(content);
-            let val = future::ready(val);
-            EagerFuture::new(val)
-        };
+        let content = EagerFuture::new(future::ready(content));
+        let parser = EagerFuture::new(future::ready(Some(Arc::new(Mutex::new(parser)))));
+        let tree = EagerFuture::new(future::ready(tree));
 
         Ok(DocumentFuture {
             uri,
@@ -72,8 +44,62 @@ impl DocumentFuture {
         })
     }
 
-    pub async fn open_from_uri(uri: lsp::Url) -> anyhow::Result<Option<Self>> {
-        todo!()
+    pub async fn open_from_uri(uri: lsp::Url) -> anyhow::Result<Self> {
+        if let Ok(path) = uri.to_file_path() {
+            let language = crate::core::Language::try_from(path.as_path())?;
+
+            let mut parser = {
+                let language = language.clone();
+                let val = tokio::spawn(async move {
+                    let val = tree_sitter::Parser::try_from(language);
+                    if let Ok(parser) = val {
+                        Some(Arc::new(Mutex::new(parser)))
+                    } else {
+                        None
+                    }
+                });
+                let val = val.map(Result::ok).map(Option::flatten);
+                let val = EagerFuture::new(val);
+                val
+            };
+
+            let text = tokio::fs::read_to_string(path).await?;
+            let content = ropey::Rope::from(text);
+
+            let tree = {
+                let parser = parser.clone();
+                let content = content.clone();
+                let byte_idx = 0;
+                let text = content.chunks().collect::<String>();
+                let old_tree = None;
+                let val = tokio::spawn(async move {
+                    if let Some(parser) = parser.await {
+                        let mut parser = parser.lock().await;
+                        parser.parse(text, old_tree).unwrap()
+                    } else {
+                        None
+                    }
+                });
+                let val = val.map(Result::ok).map(Option::flatten);
+                EagerFuture::new(val)
+            };
+
+            let content = {
+                let val = ropey::Rope::from(content);
+                let val = future::ready(val);
+                EagerFuture::new(val)
+            };
+
+            Ok(DocumentFuture {
+                uri,
+                language,
+                content,
+                parser,
+                tree,
+            })
+        } else {
+            anyhow::bail!("Could not convert uri to file path: {:#?}", uri);
+        }
     }
 
     pub async fn change<'changes>(
