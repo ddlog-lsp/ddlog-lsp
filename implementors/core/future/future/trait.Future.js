8bd1317dff36ee360791447d53bdfd66a31727de
silvanshade <silvanshade@users.noreply.github.com>
2021-11-09 20:56:36 +0000

    deploy: 67132926e3f39e0bb4199095341f92ffaf893088

---
+++ implementors/core/future/future/trait.Future.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ddlog_lsp_server"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"ddlog_lsp_server/core/struct.EagerFuture.html\" title=\"struct ddlog_lsp_server::core::EagerFuture\">EagerFuture</a>&lt;T&gt;","synthetic":false,"types":["ddlog_lsp_server::core::future::EagerFuture"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
