3eef75565ee2e88dca225f7c7b6f71cc76d10338
silvanshade <silvanshade@users.noreply.github.com>
2021-11-10 16:06:12 -0700

    Remove unused method

--- crates/server/src/core/text.rs
+++ crates/server/src/core/text.rs
@@ -6,12 +6,3 @@ pub struct Text {
     pub language: crate::core::Language,
     pub content: EagerFuture<Option<ropey::Rope>>,
 }
-
-impl Text {
-    pub fn new(
-        language_id: impl TryInto<crate::core::Language, Error = anyhow::Error>,
-        text: impl Future<Output = impl AsRef<str>>,
-    ) -> anyhow::Result<Self> {
-        todo!()
-    }
-}
