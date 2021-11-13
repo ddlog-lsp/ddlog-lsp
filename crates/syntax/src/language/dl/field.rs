efc2338e46548c476d6972f6131851ca5d227b35
silvanshade <silvanshade@users.noreply.github.com>
2021-11-12 22:11:35 -0700

    Add fields for import rule

--- crates/syntax/src/language/dl/field.rs
+++ crates/syntax/src/language/dl/field.rs
@@ -1,6 +1,8 @@
 ddlog_lsp_macros::field_ids! {
     language: "ddlog.dl",
     fields: [
+        (MODULE_ALIAS, "module_alias"),
+        (MODULE_PATH, "module_path"),
         (IDENTIFIER, "identifier"),
     ]
 }
